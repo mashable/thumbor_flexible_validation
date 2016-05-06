f2e1913faee54483ab4a4916d89dfb17b7b3a120
Chris Heald <cheald@gmail.com>
2016-05-06 12:54:31 -0700

    Update license into, rename project to thumbor_flexible_validation

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# thumbor imaging service
-# https://github.com/globocom/thumbor/wiki
+# Custom thumbor app for more flexible URL validation
+# https://github.com/mashable/thumbor_proxy
 
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
-# Copyright (c) 2011 globo.com timehome@corp.globo.com
+# Copyright (c) 2016 Mashable
 
 from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='thumbor_proxy',
+    name='thumbor_flexible_validation',
     version='1.0.1',
     packages=find_packages(),
     install_requires=[
